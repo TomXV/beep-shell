aea0956b8cb0e1f5499fb1f040f0e5a3a5c1ba12
Tom_XV <55909638+TomXV@users.noreply.github.com>
2021-07-19 14:21:46 +0900

    Create beep.sh

---
+++ beep.sh
@@ -0,0 +1,6 @@
+beep -f 2000 -l 100;beep -f 1000 -l 100
+beep -f 2000 -l 100;beep -f 1000 -l 100
+sleep 1
+beep -f 261; beep -f 293; beep -f 329; beep -f 349; beep -f 391; beep -f 440; beep -f 493; beep -f 523
+sleep 1
+beep -f 523; beep -f 493; beep -f 440; beep -f 391; beep -f 349; beep -f 329; beep -f 293; beep -f 261
